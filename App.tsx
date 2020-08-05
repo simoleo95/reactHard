7f53eb550c8c26cd605b82f58c5cbe7e33ca339d
Simon <leonardisimonemaria@gmail.com>
2020-08-05 18:34:28 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,23 +1,243 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from 'react';
+import { Text, View, Platform, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import { FontAwesome, Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+export class Feed extends React.Component {
+  state = {
+    hasPermission: null,
+    cameraType: Camera.Constants.Type.back,
+  }
+  camera: any;
+
+  async componentDidMount() {
+    this.getPermissionAsync()
+  }
+
+  getPermissionAsync = async () => {
+    // Camera roll Permission 
+    if (Platform.OS === 'ios') {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+    // Camera Permission
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasPermission: status === 'granted' });
+  }
+
+  handleCameraType=()=>{
+    const { cameraType } = this.state
+
+    this.setState({cameraType:
+      cameraType === Camera.Constants.Type.back
+      ? Camera.Constants.Type.front
+      : Camera.Constants.Type.back
+    })
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+
+    }
+  }
+
+
+  
+
+  render(){
+    const { hasPermission } = this.state
+    if (hasPermission === null) {
+      return <View />;
+    } else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+          <View style={{ flex: 1 }}>
+            <Camera style={{ flex: 1 }} type={this.state.cameraType}  ref={(ref: any) => {this.camera = ref}}>
+              <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:30}}>
+                <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent'                 
+                  }}>
+                  <Ionicons
+                      name="ios-photos"
+                      style={{ color: "#fff", fontSize: 40}}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                  onPress={()=>this.takePicture()}
+                  >
+                  <FontAwesome
+                      name="camera"
+                      style={{ color: "#fff", fontSize: 40}}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                  onPress={()=>this.handleCameraType()}
+                  >
+                  <MaterialCommunityIcons
+                      name="camera-switch"
+                      style={{ color: "#fff", fontSize: 40}}
+                  />
+                </TouchableOpacity>
+              </View>
+            </Camera>
+        </View>
+      );
+    }
+  }
+}
+
+
+
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+
+export  class Update extends React.Component {
+  state = {
+    currentLongitude: 'unknown', //Initial Longitude
+    currentLatitude: 'unknown', //Initial Latitude
+  };
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(
+      //Will give you the current location
+      position => {
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        //getting the Longitude from the location json
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        //getting the Latitude from the location json
+        this.setState({ currentLongitude: currentLongitude });
+        //Setting state Longitude to re re-render the Longitude Text
+        this.setState({ currentLatitude: currentLatitude });
+        //Setting state Latitude to re re-render the Longitude Text
+      },
+      error => alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      //Will give you the location on location change
+      console.log(position);
+      const currentLongitude = JSON.stringify(position.coords.longitude);
+      //getting the Longitude from the location json
+      const currentLatitude = JSON.stringify(position.coords.latitude);
+      //getting the Latitude from the location json
+      this.setState({ currentLongitude: currentLongitude });
+      //Setting state Longitude to re re-render the Longitude Text
+      this.setState({ currentLatitude: currentLatitude });
+      //Setting state Latitude to re re-render the Longitude Text
+    });
+  };
+  componentWillUnmount = () => {
+    navigator.geolocation.clearWatch(this.watchID);
+  };
+  render() {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+        <Text>You are Here</Text>
+        <Text
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 16,
+          }}>
+          Longitude: {this.state.currentLongitude}
+        </Text>
+        <Text
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 16,
+          }}>
+          Latitude: {this.state.currentLatitude}
+        </Text>
+      </View>
     );
   }
+}
+
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Camera',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="camera" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Update"
+        component={Update}
+        options={{
+          tabBarLabel: 'GPS',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="crosshairs-gps" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Bluetooth',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bluetooth" color={color} size={size} />
+          ),
+        }}
+      />
+
+
+
+      
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
